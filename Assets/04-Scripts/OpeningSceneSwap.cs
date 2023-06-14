ed546a485c5d18916fefe496a270fcc5d80e11f3
SCU Imaginarium (Olivia) <>
2023-06-13 23:39:34 -0700

    start screen, opening cutscene, end screen

--- Assets/04-Scripts/OpeningSceneSwap.cs
+++ Assets/04-Scripts/OpeningSceneSwap.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class NewBehaviourScript : MonoBehaviour
+public class OpeningSceneSwap : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.gameObject.tag == "exit_intro")
+        {
+            SceneManager.LoadScene(sceneName: "imaginarium");
+            Debug.Log("triggered");
+        }
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
---
+++ Assets/04-Scripts/TPtoOpening.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TPtoOpening : MonoBehaviour
+{
+    public void OnActivate()
+    {
+        SceneManager.LoadScene(sceneName: "Open");
+    }
+}
